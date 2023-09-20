e57db2be5da80be868e462d88147e9dad9bf4af1
CronusV <33589161+CronusV@users.noreply.github.com>
2023-09-20 14:21:04 -0700

    Added router to server

--- app.js
+++ app.js
@@ -5,16 +5,19 @@ const express = require('express');
 const server = express();
 const PORT = 3000;
 // router
-// TODO
+const loginRouter = require('./routes/loginRouter');
 // middleware to parse JSON from req
 const bodyParser = require('body-parser');
 server.use(bodyParser.json());
 
 // Test express
-server.use('/', (req, res) => {
-  logger.info('In test express endpoint');
-  res.send('In test server');
-});
+// server.use('/', (req, res) => {
+//   logger.info('In test express endpoint');
+//   res.send('In test server');
+// });
+
+// Using router with base /login url
+server.use('/login', loginRouter);
 // Spin up server
 server.listen(PORT, () => {
   logger.info(`Server is listening on Port: ${PORT}`);
