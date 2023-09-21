5786e4a7ef57025f50db15318337147110a2f115
CronusV <33589161+CronusV@users.noreply.github.com>
2023-09-21 13:08:41 -0700

    added functions to verify user and ticket parameters

--- routes/middleware/ticketMW.js
+++ routes/middleware/ticketMW.js
@@ -1,7 +1,9 @@
+const jwtUtil = require('../../utils/jwtUtil');
 // Check to see that ticket request is valid and has at least amount AND description
 function validateTicket(req, res, next) {
   const body = req.body;
   const invalidMessages = [];
+  // should trim to make sure we don't get something like body.amount = "    "
   if (!body.amount || body.amount === '') {
     invalidMessages.push('No amount in ticket or no amount attribute');
   }
@@ -10,7 +12,6 @@ function validateTicket(req, res, next) {
       'No description in message or no description in message'
     );
   }
-
   // Add messages if error
   if (invalidMessages.length > 0) {
     body.invalidMessage = `${invalidMessages.join(', and ')}`;
@@ -21,4 +22,45 @@ function validateTicket(req, res, next) {
   next();
 }
 
-module.exports = { validateTicket };
+function validateGetTickets(req, res, next) {
+  const body = req.body;
+  const status = req.query.status;
+  body.status = status;
+  if (status === 'pending' || status === 'approved' || status === 'denied') {
+    body.validTicket = true;
+  } else {
+    body.validTicket = false;
+    body.invalidMessage =
+      'No valid status in query parameters. Only accept, pending, approved, and denied';
+  }
+  next();
+}
+
+// Make sure we have valid token
+function validateUser(req, res, next) {
+  const body = req.body;
+  const authorizationHead = req.headers.authorization;
+  // Confirm that authorization exists and have Bearer
+  if (authorizationHead && authorizationHead.startsWith('Bearer ')) {
+    const token = authorizationHead.split(' ')[1]; // ['Bearer', <token>]
+    jwtUtil.verifyTokenAndReturnPayload(token).then((payload) => {
+      const userRole = payload.role;
+      if (userRole === 'employee' || userRole === 'manager') {
+        body.validUser = true;
+        body.role = userRole;
+      } else {
+        // Means that userRole is something we don't have implemented yet
+        body.validUser = false;
+        body.role = userRole;
+        body.invalidMessage = `Get not defined for role:${userRole}`;
+      }
+    });
+  } else {
+    // No authorization
+    body.validUser = false;
+    body.invalidMessage = 'Need Authorization header with Bearer token';
+  }
+  next();
+}
+
+module.exports = { validateTicket, validateGetTickets, validateUser };
