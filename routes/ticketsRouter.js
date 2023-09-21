979a90762861213e1d0bffc6a44033d9992d183d
CronusV <33589161+CronusV@users.noreply.github.com>
2023-09-20 22:52:34 -0700

    Added working ticket endpoint to send new tickets

--- routes/ticketsRouter.js
+++ routes/ticketsRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const uuid = require('uuid');
 // Logger
 const logger = require('../utils/logger');
 
@@ -7,5 +8,35 @@ const ticketDAO = require('../integration/ticketDAO');
 const MW = require('./middleware/ticketMW');
 
 // Assume we have user token or header
-
+router.post('/', MW.validateTicket, (req, res) => {
+  logger.info('Trying to post tickets');
+  const newTicket = req.body;
+  if (newTicket.valid) {
+    // ASSUME WE HAVE AUTHOR SOMEWHERE (probably jwt token)
+    ticketDAO
+      .addTicket(
+        uuid.v4(),
+        newTicket.amount,
+        newTicket.desc,
+        newTicket.author,
+        newTicket.type
+      )
+      .then((data) => {
+        // if valid then data is simply {}
+        logger.info(`Successful POST:\n ${JSON.stringify(newTicket)}`);
+        res.status(201).send({
+          message: 'Successfully created ticket',
+        });
+      })
+      .catch((err) => {
+        logger.info(`Failed to add ticket to dynamoDB: ${err}`);
+        res.status(400).send({ message: `Failed to add ticket in dynamoDB` });
+      });
+  } else {
+    logger.error(`Failed to post ticket because: ${newTicket.invalidMessage}`);
+    res.status(400).send({
+      message: `Failed to post ticket because: ${newTicket.invalidMessage}`,
+    });
+  }
+});
 module.exports = router;
