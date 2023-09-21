cc1db5bb141eed44e0e3648ac26c224d3bd71416
CronusV <33589161+CronusV@users.noreply.github.com>
2023-09-20 21:59:13 -0700

    Added ticketDAO function to post tickets

--- integration/ticketDAO.js
+++ integration/ticketDAO.js
@@ -8,3 +8,31 @@ AWS.config.update({
 const docClient = new AWS.DynamoDB.DocumentClient();
 // This is table name in dynamoDB
 const TableName = 'tickets-project-1';
+
+// POST tickets
+// if successful then returns {}, if not then throws error
+function addTicket(
+  ticket_id,
+  amount,
+  desc,
+  author,
+  type = 'none',
+  status = 'pending',
+  resolver_id = 0
+) {
+  const params = {
+    TableName,
+    Item: {
+      ticket_id,
+      amount,
+      desc,
+      author,
+      type,
+      status,
+      resolver_id,
+    },
+  };
+  return docClient.put(params).promise();
+}
+
+module.exports = { addTicket };
