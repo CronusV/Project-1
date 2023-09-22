1930238d7caf6f30635efebcd3907771d7c7b4eb
CronusV <33589161+CronusV@users.noreply.github.com>
2023-09-22 09:06:00 -0700

    Added logging middleware to all endpoints

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Logger
 const logger = require('./utils/logger');
+// MW
+const MW = require('./routes/middleware/logMW');
 // Init Express server
 const express = require('express');
 const server = express();
@@ -11,6 +13,7 @@ const ticketsRouter = require('./routes/ticketsRouter');
 // middleware to parse JSON from req
 const bodyParser = require('body-parser');
 server.use(bodyParser.json());
+server.use(MW.logRequest);
 
 // Using router
 server.use('/login', loginRouter);
